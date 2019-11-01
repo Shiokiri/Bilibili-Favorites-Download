af9300f9b681ca08f12e9f49b58ee6c34fe13a4c
ColorfulMist <1838001290@qq.com>
2019-11-01 17:02:36 +0800

    dev : add README and write data.py

---
+++ README.md
@@ -0,0 +1,47 @@
+# Bilibili-Favorites-Download
+
+![](https://img.shields.io/github/issues/ColorfulMist/Bilibili-Favorites-Download)
+![](https://img.shields.io/github/forks/ColorfulMist/Bilibili-Favorites-Download)
+![](https://img.shields.io/github/stars/ColorfulMist/Bilibili-Favorites-Download)
+![](https://img.shields.io/github/license/ColorfulMist/Bilibili-Favorites-Download)
+![](https://img.shields.io/github/languages/top/ColorfulMist/Bilibili-Favorites-Download)
+![](https://img.shields.io/github/last-commit/ColorfulMist/Bilibili-Favorites-Download)
+![](https://img.shields.io/github/languages/code-size/ColorfulMist/Bilibili-Favorites-Download)
+![](https://img.shields.io/github/repo-size/ColorfulMist/Bilibili-Favorites-Download)
+
+项目仍在开发中，目前仅完成部分功能，请暂时不要使用。
+
+这是一个用于下载bilibili弹幕视频网站个人收藏夹信息的爬虫，数据来源是bilibili官方网站的api和biliplus网站的api，目前支持保存成txt文本文件和可以使用Excel打开的csv逗号分隔符文件，会保存尽量多的信息种类，如有需求，可以自行对照api返回的信息进行修改。
+
+这个项目的原始动力是我自己的的需求，如果在使用中遇到了困难和bug以及有对项目的意见，欢迎在Issues中提出，我将提供帮助和不断改进。
+
+如果这个项目帮助了你，欢迎Star，您的Star是我继续维护与开发的动力。
+
+## 使用
+
+请首先将您需要爬取信息的bilibili收藏夹设为公开
+
+### 使用python源代码
+
+需要`Python3`环境
+
+`data.py`是输出txt文件的代码
+
+`data_csv.py`是输出csv文件的代码
+
+在命令行下输入`python data.py`后
+
+输入你的bilibili账号的uid，uid是web端个人主页网址url末尾的数字，或者可以自行查看bilibili个人信息
+
+收藏夹信息文件会保存到当前目录
+
+### 使用exe可执行文件
+
+在release中提供`data.exe`及`data_csv.exe`
+
+打开exe文件后输入bilibili账号的uid
+
+
+## 开发计划
+
+制作使用PyQt5的窗口程序，并开发选择保存文件路径，选择两种输出方式的功能。
--- data.py
+++ data.py
@@ -15,7 +15,7 @@ def initUid():
 def getJsonUrl(url):
     Data = urlopen(url).read().decode('utf-8')
     jsonData = json.loads(Data)
-    return Data
+    return jsonData
 
 def getFavoriteList(uid):
     global Favorite_id, Favorite_name, Fav_tot
@@ -26,7 +26,7 @@ def getFavoriteList(uid):
     for i in range(0, Fav_tot):
         Favorite_id.append(listInfo[i]['fid'])
         Favorite_name.append(listInfo[i]['name'])
-        print('收藏夹编号:%d fid:%d 收藏夹名称:%s 收藏夹视频数量' % (i+1, Favorite_id[i] ,Favorite_name[i], listInfo[i]['cur_count']))
+        print('收藏夹编号:%d fid:%d 名称:%s 视频数量:%d\n' % (i+1, Favorite_id[i] ,Favorite_name[i], listInfo[i]['cur_count']))
         getFavListVideo(uid, Favorite_id[i])
 
 aFavCnt = 0
@@ -49,26 +49,59 @@ def printVideoInfo(aPageInfo):
     global aFavCnt
     for i in range(0, len(aPageInfo)):
         aFavCnt += 1
+        video_id = aPageInfo[i]['aid']
         if aPageInfo[i]['title'] == '已失效视频':
-            gitInvalidVideoInfo(aPageInfo[i]['aid'])
+            gitInvalidVideoInfo(video_id, aPageInfo[i])
         else:
-            print('%d %d %s' % (aFavCnt, aPageInfo[i]['aid'],aPageInfo[i]['title']))
+            InvData = bilibiliVideoApi(video_id)
+            print('视频编号：%d  AV号：%d  视频标题:%s' % (aFavCnt, aPageInfo[i]['aid'],aPageInfo[i]['title']))
+            # print('封面图片:%s' % ())
 
-def gitInvalidVideoInfo(video_id):
+
+def gitInvalidVideoInfo(video_id, Info):
     global aFavCnt
     url = 'https://www.biliplus.com/api/view?id={vid}'.format(vid=video_id)
     InvVideoInfo = getJsonUrl(url)
     if 'code' in InvVideoInfo:
-        print('%d %d [已失效][BiliplusApi数据缺失] 已失效视频' % (aFavCnt, video_id))
+        print('[已失效][BiliplusApi数据缺失] 视频编号：%d  AV号：%d  视频标题:已失效视频' % (aFavCnt, video_id))
+        print('数据最后获取日期:%s' % (Info['lastupdate']))
+        print('描述:%s' % (Info['desc']))
+        print('封面图片:%s' % (Info['pic']))
+        print('分区:%s' % (Info['tname']))
+        print('标签:%s' % (Info['dynamic']))
+        print('up主用户名:%s up主uid:%s' % (Info['owner']['name'], Info['owner']['mid']))
+        print('投稿时间:%s' % (Info['ctime']))
+        print('播放:%s 弹幕:%s 回复:%s 收藏:%s 硬币:%s 分享:%s 喜欢:%s' % (Info['v2_app_api']['stat']['view'], Info['v2_app_api']['stat']['danmaku'], Info['v2_app_api']['stat']['reply'], Info['v2_app_api']['stat']['favorite'], Info['v2_app_api']['stat']['coin'], Info['v2_app_api']['stat']['share'], InvVideoInfo['v2_app_api']['stat']['like'],))
+        print('收藏日期%s' % (Info['fav_at']))
+        print()
+        input()
         return
-    print('%d %d [已失效][BiliplusApi获得数据] %s' % (aFavCnt, video_id, InvVideoInfo['title']))
+    print('[已失效][BiliplusApi获得数据] 视频编号：%d  AV号：%d  视频标题：%s' % (aFavCnt, video_id, InvVideoInfo['title']))
+    print('数据最后获取日期:%s' % (InvVideoInfo['lastupdate']))
+    print('描述:%s' % (InvVideoInfo['description']))
+    print('封面图片:%s' % (InvVideoInfo['pic']))
+    print('分区:%s' % (InvVideoInfo['typename']))
+    print('标签:%s' % (InvVideoInfo['tag']))
+    print('up主用户名:%s up主uid:%s' % (InvVideoInfo['author'], InvVideoInfo['mid']))
+    print('投稿时间:%s' % (InvVideoInfo['created_at']))
+    print('播放:%s 弹幕:%s 回复:%s 收藏:%s 硬币:%s 分享:%s 喜欢:%s' % (InvVideoInfo['v2_app_api']['stat']['view'], InvVideoInfo['v2_app_api']['stat']['danmaku'], InvVideoInfo['v2_app_api']['stat']['reply'], InvVideoInfo['v2_app_api']['stat']['favorite'], InvVideoInfo['v2_app_api']['stat']['coin'], InvVideoInfo['v2_app_api']['stat']['share'], InvVideoInfo['v2_app_api']['stat']['like'],))
+    print('收藏日期%s' % (Info['fav_at']))
+    print()
+
+def bilibiliVideoApi(video_id):
+    url = 'https://api.bilibili.com/x/article/archives?ids={uid}'.format(uid=video_id)
+    videoInfo = getJsonUrl(url)
+    return videoInfo
 
 
-def Start():
+def start(): # for cmd
     initUid()
-    fo = open("FavoriteVideoList.txt", 'w', encoding = 'utf-8')
+    fileWrite = open("FavoriteVideoList.txt", 'w', encoding='utf-8')
     getFavoriteList(bilibili_uid)
-    fo.close()
+    fileWrite.close()
 
-if __name__ == '__main__':
-    Start()
+# def windowMain(Uid, filePath): # for window ui
+
+
+if __name__ == '__main__': # cmd
+    start()
