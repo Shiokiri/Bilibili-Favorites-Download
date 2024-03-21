3f36349ba3992e493b42ef77e410f1706f2aeb0d
Shiokiri <xluoying2017@gmail.com>
2024-03-21 22:17:51 +0800

    fix: add a request header

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
-from urllib.request import urlopen
+import urllib.request
 import json
-import re
 import time
 import sys
 import logging
@@ -11,11 +10,14 @@ bilibili_uid = 0
 aFavVideoCnt = 0
 totVideoCnt = 0
 totVideoNum = 0
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
+
 
 def getJsonUrl(url):
-    Data = urlopen(url).read().decode('utf-8')
+    Data = urllib.request.urlopen(urllib.request.Request(url=url, headers=headers)).read().decode('utf-8')
     jsonData = json.loads(Data)
     return jsonData
+
 
 def getFavoriteList(uid):
     global totVideoNum
@@ -30,6 +32,7 @@ def getFavoriteList(uid):
         print('收藏夹编号:#%d 名称:%s 视频数量:%d\n' % (i + 1, listInfo[i]['title'], listInfo[i]['media_count']))
         getFavListVideo(listInfo[i]['id'], int(listInfo[i]['media_count'] / 20 + 1))
 
+
 def getFavListVideo(media_id, pagecount):
     global aFavVideoCnt, totVideoCnt, totVideoNum
     aFavVideoCnt = 0
@@ -39,6 +42,7 @@ def getFavListVideo(media_id, pagecount):
             media_id=media_id, page=i)
         favPageDate = getJsonUrl(favPageUrl)
         printVideoInfo(favPageDate['data']['medias'])
+
 
 def printVideoInfo(aPageInfo):
     global aFavVideoCnt, totVideoCnt, totVideoNum
@@ -56,6 +60,7 @@ def printVideoInfo(aPageInfo):
         print('封面图片:%s' % (Info['cover']))
         printInfo(Info)
 
+
 def printInfo(Info):
     print('投稿时间:' + time.strftime("%Y-%m-%d-%H:%M:%S", time.gmtime(Info['pubtime'])))
     print('描述:%s' % (Info['intro']))
@@ -70,8 +75,10 @@ def printInfo(Info):
     print('收藏时间:' + time.strftime("%Y-%m-%d-%H:%M:%S", time.gmtime(Info['fav_time'])))
     print()
 
+
 biliplusApiCnt = 0
 runTime = 0
+
 
 def getInvalidVideoInfo(video_id, Info):
     global aFavVideoCnt, biliplusApiCnt, runTime
@@ -89,13 +96,14 @@ def getInvalidVideoInfo(video_id, Info):
         print('封面图片:%s' % (InvVideoInfo['pic']))
         printInfo(Info)
 
+
 def start():
     global totVideoNum, runTime, bilibili_uid
     logging.basicConfig(level=logging.DEBUG)
     if sys.getdefaultencoding() == 'ascii':
-        sys.stdout = open("FavoriteVideoList.txt", "w", encoding='gb2312')
+        sys.stdout = open("favorites.txt", "w", encoding='gb2312')
     else:
-        sys.stdout = open("FavoriteVideoList.txt", "w", encoding='utf-8')
+        sys.stdout = open("favorites.txt", "w", encoding='utf-8')
     bilibili_uid = int(input())
     runTime = time.time()
     getFavoriteList(bilibili_uid)
